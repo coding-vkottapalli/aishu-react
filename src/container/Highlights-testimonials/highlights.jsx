19aa606b76ccb0045e2797c4133d769457a0376d
Vivek <vivek_wolves@yahoo.com>
2022-10-02 22:27:52 -0700

    Navigation and Home page

---
+++ src/container/Highlights-testimonials/highlights.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./highlights.scss";
+const Highlights = () => {
+  return <div>Highlights</div>;
+};
+
+export default Highlights;
---
+++ src/container/index.js
@@ -0,0 +1,6 @@
+export { default as About } from "./About/about.jsx";
+export { default as Footer } from "./Footer/footer.jsx";
+export { default as Header } from "./Header/header.jsx";
+export { default as Highlights } from "./Highlights-testimonials/highlights.jsx";
+export { default as Skills } from "./Skills/skills.jsx";
+export { default as Work } from "./Work/work.jsx";
